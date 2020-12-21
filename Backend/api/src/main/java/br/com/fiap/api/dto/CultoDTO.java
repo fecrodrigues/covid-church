e552df4cd49d41d6fd9589f983acbb710a64bf0b
sheldongomes <68485686+sheldongomes@users.noreply.github.com>
2020-12-21 00:15:07 -0300

    Agendamentos Usuario

---
+++ Backend/api/src/main/java/br/com/fiap/api/dto/CultoDTO.java
@@ -0,0 +1,28 @@
+package br.com.fiap.api.dto;
+
+import br.com.fiap.api.model.CultoModel;
+
+import java.util.Date;
+
+public class CultoDTO {
+
+    private String id;
+    private String idInstituicao;
+    private String descricao;
+    private Date data;
+    private Integer duracao;
+    private Integer capacidade;
+    private Integer vagas;
+
+    public CultoDTO() {}
+
+    public CultoDTO(CultoModel model) {
+        this.id = model.getId();
+        this.idInstituicao = model.getIdInstituicao();
+        this.descricao = model.getDescricao();
+        this.data = model.getData();
+        this.duracao = model.getDuracao();
+        this.capacidade = model.getCapacidade();
+        this.vagas = vagas;
+    }
+}
