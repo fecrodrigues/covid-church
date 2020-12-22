26f32b49277e6ce598d53df4763b679283fa2e9f
Breno Marino de Souza Ferreira <breno_marino@hotmail.com>
2020-12-21 23:03:59 -0300

    subindo parte de security

---
+++ Backend/api/src/main/java/br/com/fiap/api/security/JwtUserDetails.java
@@ -0,0 +1,34 @@
+package br.com.fiap.api.security;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import br.com.fiap.api.model.Usuario;
+import br.com.fiap.api.repository.UsuarioRepository;
+
+@Component
+public class JwtUserDetails implements UserDetailsService{
+
+    private UsuarioRepository userRepository;
+
+    public JwtUserDetails(UsuarioRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Usuario user = userRepository.findFirstByUserName(userName);
+        if(user == null){
+            throw new UsernameNotFoundException("usuario.nao.encontrado");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                new ArrayList<>() // roles
+        );
+    }
+}
